fa28b8dd374c3dc7e5d48ae4ad7a170d4699da2c
inverse <inversealtruism@gmail.com>
2023-05-17 19:47:06 +0200

    Staking Buttons

--- public/index.js
+++ public/index.js
@@ -1,36 +1,350 @@
 window.addEventListener('DOMContentLoaded', () => {
+  let provider;
+  let signer;
+  let stakingContract;
+
+  const stakingContractABI = [
+	{
+		"inputs": [],
+		"name": "exit",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getReward",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "_plsDoge",
+				"type": "address"
+			},
+			{
+				"internalType": "contract IERC20",
+				"name": "_plsBone",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "reward",
+				"type": "uint256"
+			}
+		],
+		"name": "RewardPaid",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "stake",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "Staked",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "Withdrawn",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "earned",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lastUpdatedBlock",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "plsBone",
+		"outputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "plsDoge",
+		"outputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "rewardPerToken",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "rewardPerTokenStored",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "rewardRate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "rewards",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "userRewardPerTokenPaid",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
   // Connect Wallet button click event
   const connectButton = document.getElementById('connectButton');
-  let isConnected = false;
-
   connectButton.addEventListener('click', async () => {
     // Check if MetaMask is installed
     if (window.ethereum) {
-      if (!isConnected) {
-        try {
-          // Request user's permission to connect
-          await window.ethereum.enable();
-          console.log('Wallet connected successfully!');
-          isConnected = true;
-          connectButton.innerText = 'Disconnect Wallet';
-          // You can perform further actions with the connected wallet here
-        } catch (error) {
-          console.error('Failed to connect wallet:', error);
-        }
-      } else {
-        try {
-          // Disconnect wallet
-          // As of my knowledge cutoff in September 2021, there's no built-in method to disconnect a MetaMask wallet
-          // A workaround is to reload the page or to clear the currently connected account
-          window.location.reload();
-          // or
-          // window.ethereum.selectedAddress = null;
-        } catch (error) {
-          console.error('Failed to disconnect wallet:', error);
-        }
+      try {
+        // Request user's permission to connect
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        console.log('Wallet connected successfully!');
+
+        // Initialize ethers.js and contracts after connecting the wallet
+        provider = new ethers.providers.Web3Provider(window.ethereum);
+        signer = provider.getSigner();
+        stakingContract = new ethers.Contract('0x973da004D844384f7Cf1f5C338f61f970fAA4423', stakingContractABI, signer);
+
+      } catch (error) {
+        console.error('Failed to connect wallet:', error);
       }
     } else {
       console.error('MetaMask extension not detected!');
+    }
+  });
+
+  // Stake button click event
+  const stakeButton = document.getElementById('stakeButton');
+  stakeButton.addEventListener('click', async () => {
+    const stakeAmount = document.getElementById('stakeAmount').value;
+    try {
+      // Call the staking function on your contract
+      const tx = await stakingContract.stake(ethers.utils.parseEther(stakeAmount));
+      await tx.wait();
+      console.log('Staking transaction successful!');
+    } catch (error) {
+      console.error('Failed to stake:', error);
+    }
+  });
+
+  // Redeem button click event
+  const redeemButton = document.getElementById('redeemButton');
+  redeemButton.addEventListener('click', async () => {
+    try {
+      // Call the redeem function on your contract
+      const tx = await stakingContract.getReward();
+      await tx.wait();
+      console.log('Redeem transaction successful!');
+    } catch (error) {
+      console.error('Failed to redeem:', error);
+    }
+  });
+
+  // Disconnect button click event
+  const disconnectButton = document.getElementById('disconnectButton');
+  disconnectButton.addEventListener('click', async () => {
+    // Disconnect the wallet
+    try {
+      await window.ethereum.request({ method: 'wallet_requestPermissions', params: [{ eth_accounts: {} }] });
+      console.log('Wallet disconnected successfully!');
+    } catch (error) {
+      console.error('Failed to disconnect wallet:', error);
     }
   });
 });
